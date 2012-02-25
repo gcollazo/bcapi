2acc90585c246bca26acb4a72d35e8140a8f3062
Giovanni Collazo <gcollazo@gmail.com>
2012-02-25 13:35:59 -0400

    Added logic to get mongohq credentials from heroku env

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os, sys, urlparse
+
 # App Settings
 DEBUG = True
 SECRET_KEY = 'vwuVh1sQ9/QbT_kj SOR!*;44m%@&U%~kGY'
@@ -6,3 +8,12 @@ SECRET_KEY = 'vwuVh1sQ9/QbT_kj SOR!*;44m%@&U%~kGY'
 DB_HOST = 'localhost'
 DB_PORT = 27017
 DB_NAME = 'bcapi'
+
+try:
+    if os.environ.has_key('MONGOHQ_URL'):
+        url = urlparse.urlparse(os.environ['MONGOHQ_URL'])
+        print url.path.split('/')[-1]
+        print url.username
+        print url.password
+except:
+    print "Unexpected error:", sys.exc_info()
