7236a86525a6872dcd772ddefdfc5f00e0c93bfc
Giovanni Collazo <gcollazo@gmail.com>
2012-02-25 14:43:03 -0400

    Removed unecessary comment

--- settings.py
+++ settings.py
@@ -9,7 +9,6 @@ else:
 SECRET_KEY = 'vwuVh1sQ9/QbT_kj SOR!*;44m%@&U%~kGY'
 
 # Database Settings
-# Current db URI: mongodb://heroku:0fe0439c319ae6239411f9a7733d63d4@staff.mongohq.com:10032/app3030653
 DB_URI = 'mongodb://localhost:27017/bcapi'
 DB_NAME = parse_uri(DB_URI)['database']
 
